e88c1506dd4ed34a53b27581d7aaa6693bdaa6f3
Marisol <s.min.g7904@gmail.com>
2024-12-24 11:02:53 +0900

    initial commit

---
+++ src/main/java/com/registration/lecture/hhpluslectureregistration/HhplusLectureRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.registration.lecture.hhpluslectureregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusLectureRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusLectureRegistrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/registration/lecture/hhpluslectureregistration/HhplusLectureRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registration.lecture.hhpluslectureregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusLectureRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
